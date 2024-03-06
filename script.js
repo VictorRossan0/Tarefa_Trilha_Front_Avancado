1fc5132c2509741b77a4caa10223d48802f6dd63
VictorRossan0 <victor.amaral@globalhitss.com.br>
2024-03-06 13:16:13 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+const themeToggleBtn = document.getElementById('toggle-btn');
+
+let darkMode = false;
+
+themeToggleBtn.addEventListener('click', () => {
+  darkMode = !darkMode;
+  const container = document.querySelector('.container');
+  if (darkMode) {
+    container.classList.add('dark-mode');
+    themeToggleBtn.textContent = 'Modo Claro';
+  } else {
+    container.classList.remove('dark-mode');
+    themeToggleBtn.textContent = 'Modo Escuro';
+  }
+});
+
+addTaskBtn.addEventListener('click', () => {
+  const taskText = taskInput.value.trim();
+  if (taskText === '') return;
+
+  const existingTasks = Array.from(taskList.children).map(task => task.textContent);
+  if (existingTasks.includes(taskText)) {
+    alert('Esta tarefa já existe na lista.');
+    return;
+  }
+
+  const taskItem = document.createElement('li');
+  taskItem.textContent = taskText;
+
+  taskItem.addEventListener('click', () => {
+    taskItem.classList.toggle('completed');
+  });
+
+  taskItem.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    taskItem.remove();
+  });
+
+  taskList.appendChild(taskItem);
+  taskInput.value = '';
+});
